aa4ed5165c9ba5cc450d5552f9d38cdd5341c27f
HED2084 <wes.hediger@gmail.com>
2020-02-24 15:09:15 -0600

    remove demo folder, change static main syntax

--- src/main/java/com/hediger/recipes/ReactAndSpringDataRestApplication.java
+++ src/main/java/com/hediger/recipes/ReactAndSpringDataRestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReactAndSpringDataRestApplication {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
 	}
 }
