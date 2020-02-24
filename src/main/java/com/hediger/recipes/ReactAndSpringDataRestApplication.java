7c168f844e8f5f454c80a7f54d13c53b922d2b7a
HED2084 <wes.hediger@gmail.com>
2020-02-24 14:44:14 -0600

    add imports

--- src/main/java/com/hediger/recipes/ReactAndSpringDataRestApplication.java
+++ src/main/java/com/hediger/recipes/ReactAndSpringDataRestApplication.java
@@ -1,5 +1,8 @@
 package com.hediger.recipes;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ReactAndSpringDataRestApplication {
 
