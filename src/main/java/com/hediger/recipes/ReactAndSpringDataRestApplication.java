492280847df56f1579ecd255e3229d8da2b2d7e0
HED2084 <wes.hediger@gmail.com>
2020-02-24 14:25:11 -0600

    add reactandspringdatarestaplication.java

---
+++ src/main/java/com/hediger/recipes/ReactAndSpringDataRestApplication.java
@@ -0,0 +1,9 @@
+package com.hediger.recipes;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestApplication {
+
+	public static void main(String args[]) {
+		SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+	}
+}
