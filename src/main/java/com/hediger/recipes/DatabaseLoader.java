51ba23425ca2e53e7369fb250c113f9c0c86aac4
HED2084 <wes.hediger@gmail.com>
2020-02-24 14:41:59 -0600

    add imports

--- src/main/java/com/hediger/recipes/DatabaseLoader.java
+++ src/main/java/com/hediger/recipes/DatabaseLoader.java
@@ -1,5 +1,10 @@
 package com.hediger.recipes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
 public class DatabaseLoader implements CommandLineRunner {
 
 	private final RecipeRepository repository;
