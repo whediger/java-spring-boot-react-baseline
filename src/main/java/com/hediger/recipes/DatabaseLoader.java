9eb8f6edaa5524126768bd3c1b946f874999a9e1
HED2084 <wes.hediger@gmail.com>
2020-02-24 14:05:53 -0600

    add databaseloader

---
+++ src/main/java/com/hediger/recipes/DatabaseLoader.java
@@ -0,0 +1,16 @@
+
+
+public class DatabaseLoader impliments CommandLineRunner {
+
+	private final RecipeRepository repository;
+
+	@Autowired
+	public DatabaseLoader(RecipeRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public void run(String... strings) throws exception {
+		this.repository.save(new Recipe("cheddar Cheese", "open package and eat it", "cheddar from store"))
+	}
+}
