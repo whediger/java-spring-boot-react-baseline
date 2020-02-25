0c3cd1cbfefe297ecd15557b1cfd1b350d938ec9
HED2084 <wes.hediger@gmail.com>
2020-02-25 13:04:38 -0600

    add package and imports

--- src/main/java/com/hediger/recipes/HomeController.java
+++ src/main/java/com/hediger/recipes/HomeController.java
@@ -1,4 +1,9 @@
 // https://spring.io/guides/tutorials/react-and-spring-data-rest/
+package com.hediger.recipes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class HomeController {
@@ -6,7 +11,7 @@ public class HomeController {
 	@RequestMapping(value="/")
 	public String index() {
 		//TODO: remove scaffolding
-		System.out.println("index route hit +===}========>")
+		System.out.println("index route hit +===}========>");
 		return "index";
 	}
 }
