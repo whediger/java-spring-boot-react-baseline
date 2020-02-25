9184b65f7ae99483fa6eeb0f92fa4b0802e1a30a
HED2084 <wes.hediger@gmail.com>
2020-02-25 14:12:31 -0600

    add RecipeList

---
+++ src/main/js/app.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'rect-dom'
+import client from './client'
+
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state: = {recipes: []};
+	}
+
+	componentsDidMount() {
+		client({method: 'GET', path: '/api/recipes'}).done(response => {
+			this.setState({recipes: response.entity._embedded.recipes})
+		})
+	}
+
+	render() {
+		return <RecipeList recipes={this.state.recipes} />
+	}
+}
+
+class EmployeeList extends React.Component {
+
+	render() {
+		const recipes = this.props.recipes.map(recipe =>
+			<Recipe key={recipe._links.self.href} employee={employee} />
+		);
+
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>title</th>
+						<th>description</th>
+						<th>ingredient</th>
+					</tr>
+				</tbody>
+				{recipes}
+			</table>
+		)
+	}
+}
