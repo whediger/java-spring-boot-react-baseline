0a0319bef0ff443e7bbce30d203d614d211378c6
HED2084 <wes.hediger@gmail.com>
2020-02-28 21:21:42 -0600

    add WebSocketConfiguration

---
+++ src/main/java/com/hediger/recipes/WebSocketConfiguration.java
@@ -0,0 +1,25 @@
+package com.hediger.recipes;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndPointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Component
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+	static final String MESSAGE_PREFIX = "/topic";
+
+	@Override
+	public void registerStompEndpoints(StompEndPointRegistry registry) {
+		registry.addEndpoint("/recipes").withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker(MESSAGE_PREFIX);
+		registry.setApplicationDestinationPrefixes("/app");
+	}
+}
