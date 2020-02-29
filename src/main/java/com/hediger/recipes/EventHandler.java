e3bda9bcad6e1be4318a085b6ec66455a82fbe6e
HED2084 <wes.hediger@gmail.com>
2020-02-29 16:33:38 -0600

    add EventHandler

---
+++ src/main/java/com/hediger/recipes/EventHandler.java
@@ -0,0 +1,53 @@
+package com.hediger.recipes;
+
+import static com.hediger.recipes.WebSocketConfiguration.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.HandleAfterCreate;
+import org.springframework.data.rest.core.annotation.HandleAfterDelete;
+import org.springframework.data.rest.core.annotation.HandleAfterSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.hateoas.server.Entitylinks;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RepositoryEventHandler(Recipe.class)
+public class EventHandler {
+
+	private final SimpMessagingTemplate websocket;
+
+	private final EntityLinks entityLinks;
+
+	@Autowired
+	public EventHandler(SimpMessagingTemplate websocket, EntityLinks entityLinks) {
+		this.websocket = websocket;
+		this.entityLinks = entityLinks;
+	}
+
+	@HandleAfterCreate
+	public void newRecipe(Recipe recipe) {
+		this.websocket.convertAndSend(
+			MESSAGE_PREFIX + "/newRecipe", getPath(recipe)
+		);
+	}
+
+	@HandleAfterDelete
+	public void deleteRecipe(Recipe recipe) {
+		this.websocket.convertAndSend(
+			MESSAGE_PREFIX + "/deleteRecipe", getPath(recipe)
+		);
+	}
+
+	@HandleAfterSave
+	public void updateRecipe(Recipe recipe) {
+		this.websocket.convertAndSend(
+			MESSAGE_PREFIX+ "/updateRecipe", getPath(recipe)
+		);
+	}
+
+	private String getPath(Recipe recipe) {
+		return this.entityLinks.linkForResource(recipe.getClass(),
+			recipe.getId()).toUri().getPath();
+	}
+}
