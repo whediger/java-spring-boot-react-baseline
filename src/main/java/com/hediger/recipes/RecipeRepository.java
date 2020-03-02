04a8ca982fecbfec90aec033c5f09c366981404f
HED2084 <wes.hediger@gmail.com>
2020-03-02 17:46:36 -0600

    add chef relationships

--- src/main/java/com/hediger/recipes/RecipeRepository.java
+++ src/main/java/com/hediger/recipes/RecipeRepository.java
@@ -1,7 +1,21 @@
 package com.hediger.recipes;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface RecipeRepository extends PagingAndSortingRepository<Recipe, Long> {
+
+	@Override
+	@PreAuthorize("#recipe?.chef == null or #recipe?.chef?.name == authentication?.name")
+	Recipe save(@Param("recipe") Recipe recipe);
+
+	@Override
+	@PreAuthorize("@recipeRepository.findById(#id)?.chef?.name == authentication?.name")
+	void deleteById(@Param("id") Long id);
+
+	@Override
+	@PreAuthorize("#recipe?.chef?.name == authentication?.name")
+	void delete(@Param("recipe") Recipe recipe);
 
 }
