7370037d1c36780b47f55064f63c8eb1394829b5
HED2084 <wes.hediger@gmail.com>
2020-03-10 20:07:43 -0500

    remove scafolding

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -9,13 +9,8 @@ const combined = combineReducers({ recipe, loggedInChef })
 
 const store = createStore(combined, { loggedInChef: { name: document.getElementById('chefname').innerHTML }})
 
-console.log("___-------CHEF VALUE _----get state---->>>>>>>");
-console.log(store.getState());
-
 window.React = React
 window.store = store
-
-		//<App loggedInChef={document.getElementById('chefname').innerHTML}/>
 
 render (
 	<Provider store={store}>
